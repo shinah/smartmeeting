200a792a063410e0156fbb996309f638a9ca96ca
onmds <onmds@naver.com>
2018-05-07 00:46:59 +0900

    채팅방 연결(수정 필요)

--- Scripts/activate.bat
+++ Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\shinah\Desktop\smartmeeting"
+set "VIRTUAL_ENV=C:\Users\onmds\Desktop\smartmeeting"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
